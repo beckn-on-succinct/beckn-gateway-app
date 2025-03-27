daf4c7536c570979e8f6762482f36bf34326a02a
Venky <venkatramanm@gmail.com>
2025-03-28 00:21:52 +0530

    Reset id for postgres

--- src/main/java/in/succinct/standalone/configuration/AppInstaller.java
+++ src/main/java/in/succinct/standalone/configuration/AppInstaller.java
@@ -1,10 +1,12 @@
 package in.succinct.standalone.configuration;
 
 import com.venky.swf.configuration.Installer;
+import com.venky.swf.db.Database;
+
 public class AppInstaller implements Installer{
 
   public void install() {
-    
+    Database.getInstance().resetIdGeneration();
   }
 }
 
